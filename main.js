9c450a60443c647c80b64cba45a475fd923ca753
Dave Fontenot <dave@hackmatch.com>
2015-09-05 22:25:12 -0400

    included node slack client and a main.js that prints out channels fontebot is in

---
+++ main.js
@@ -0,0 +1,33 @@
+	
+var Slack = require('slack-client');
+ 
+var token = 'xoxb-10252713491-3YGR89NeLtYRDAh1DCbEA28U';
+ 
+var slack = new Slack(token, true, true);
+ 
+slack.on('open', function () {
+    var channels = Object.keys(slack.channels)
+        .map(function (k) { return slack.channels[k]; })
+        .filter(function (c) { return c.is_member; })
+        .map(function (c) { return c.name; });
+ 
+    var groups = Object.keys(slack.groups)
+        .map(function (k) { return slack.groups[k]; })
+        .filter(function (g) { return g.is_open && !g.is_archived; })
+        .map(function (g) { return g.name; });
+ 
+    console.log('Welcome to Slack. You are ' + slack.self.name + ' of ' + slack.team.name);
+ 
+    if (channels.length > 0) {
+        console.log('You are in: ' + channels.join(', '));
+    }
+    else {
+        console.log('You are not in any channels.');
+    }
+ 
+    if (groups.length > 0) {
+       console.log('As well as: ' + groups.join(', '));
+    }
+});
+ 
+slack.login();
