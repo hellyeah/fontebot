12db21c52933e21be0e7612d45f8df866b42a10b
Dave Fontenot <dave@hackmatch.com>
2015-09-06 01:05:16 -0400

    kartik replaced < > special characters with html entities and now tagging works

--- main.js
+++ main.js
@@ -1,45 +1,25 @@
 	
 var Slack = require('slack-client');
  
-var token = 'xoxb-10252713491-3YGR89NeLtYRDAh1DCbEA28U';
+var token = 'MY SUPER SECRET BOT TOKEN';
  
 var slack = new Slack(token, true, true);
- 
-slack.on('open', function () {
-    var channels = Object.keys(slack.channels)
-        .map(function (k) { return slack.channels[k]; })
-        .filter(function (c) { return c.is_member; })
-        .map(function (c) { return c.name; });
- 
-    var groups = Object.keys(slack.groups)
-        .map(function (k) { return slack.groups[k]; })
-        .filter(function (g) { return g.is_open && !g.is_archived; })
-        .map(function (g) { return g.name; });
- 
-    console.log('Welcome to Slack. You are ' + slack.self.name + ' of ' + slack.team.name);
- 
-    if (channels.length > 0) {
-        console.log('You are in: ' + channels.join(', '));
-    }
-    else {
-        console.log('You are not in any channels.');
-    }
- 
-    if (groups.length > 0) {
-       console.log('As well as: ' + groups.join(', '));
-    }
-});
- 
-slack.login();
 
 slack.on('message', function(message) {
     var channel = slack.getChannelGroupOrDMByID(message.channel);
     var user = slack.getUserByID(message.user);
-
-    if (message.type === 'message') {
+ 
+    if (message.type === 'message' && !user.is_bot) {
         console.log(channel.name + ':' + user.name + ':' + message.text);
         if (message.text.indexOf("has joined the channel") > -1) {
-            console.log('Welcome @' + user.name + '! What are you building!?');
+            var indexOfUserNameEnding = message.text.indexOf('>')
+            if (indexOfUserNameEnding > -1) {
+                var userNameAndID = message.text.substr(0,indexOfUserNameEnding+1).replace('&lt;', '<').replace('&gt;', '>')
+                channel.send('Welcome ' + userNameAndID + '! What are you building!?')
+            }
         }
     }
 });
+
+slack.login();
+
