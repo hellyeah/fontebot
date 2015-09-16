b138a449f131f793be896bc757aff0ea294d0212
Sam Couch <samcouch91@gmail.com>
2015-09-16 15:48:56 -0400

    update welcome and make plus plus work with emojis

--- scripts/welcome.js
+++ scripts/welcome.js
@@ -9,16 +9,18 @@ var scheduler = require('node-schedule')
 
 module.exports = function(robot){
   var WELCOME_ROOMS = ['general']
-  var WELCOME_MESSAGES = ['What are you working on?', 'Tell us about yourself!', 'Anything we can help you with?']
+  var WELCOME_MESSAGES = ['Working on anything interesting?', 'Tell us about yourself!', 'Anything we can help you with?']
 
-  /** check the welcome queue every 10 minutes */
-  var welcomeSchedule = scheduler.scheduleJob('*/10 * * * *', function() {
+  /** check the welcome queue every 5 minutes */
+  var rule =  new scheduler.RecurrenceRule()
+  rule.minute = new scheduler.Range(0, 59, 5)
+  var welcomeSchedule = scheduler.scheduleJob(rule, function() {
       welcomeUsers()
-  });
+  })
 
   /** Build the slack-specific string for @ replying users. */
   function buildUser(user){
-    return '<@' + user.id + '|' + user.name + '>'
+    return '<@' + user.id + '>'
   }
 
   /** return array of user strings to welcome */
@@ -31,7 +33,7 @@ module.exports = function(robot){
   function welcomeUsers(){
     var users = getUsers().join(', ')
     if (!_.isEmpty(users)){
-      var message = 'Welcome ' + users + '! ' + _.sample(WELCOME_MESSAGES)
+      var message = 'Welcome ' + users + '! ' + WELCOME_MESSAGES[nextWelcomeMessageIndex()]
 
       /** clear out the queue */
       updateBrain([])
@@ -51,11 +53,17 @@ module.exports = function(robot){
     //ensure only unique users get added
     if(_.isUndefined(_.find(queue, {id: user.id}))){
       queue.push({
-        id: user.id,
-        name: user.name
+        id: user.id
       })
       updateBrain(queue)
     }
+  }
+
+    /**  Get and increment the welcome message index. */
+  function nextWelcomeMessageIndex(){
+    lastMessageIndex = (robot.brain.get("last_welcome_index") || 0) % WELCOME_MESSAGES.length;
+    robot.brain.set("last_welcome_index", lastMessageIndex + 1);
+    return lastMessageIndex;
   }
 
   /** set the welcome_queue */
